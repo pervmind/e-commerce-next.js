e96c3d695a85bb35f1e83432d1d39ac183b49ad9
pervmind <3msabdullah@gmail.com>
2023-02-07 15:25:25 +0200

    responsive background

--- e-commerce-app/src/pages/index.tsx
+++ e-commerce-app/src/pages/index.tsx
@@ -1,14 +1,15 @@
 import HeadComponent from '../components/Head'
-import styles from '@/styles/Home.module.css'
-import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
       <HeadComponent />
       <main>
-        <div className={styles.background}>
-          <Image src="/homeWalpaper.jpg"alt="wallpaper" layout='responsive' width={1920} height={1080} className='home-background'/>
+        <div className={styles.container}>
+          <div className={styles.background}>
+            
+          </div>
         </div>
       </main>
     </>
