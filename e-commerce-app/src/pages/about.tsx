3800a2a91034522443af1116593b33314a252131
pervmind <3msabdullah@gmail.com>
2023-02-08 13:22:30 +0200

    policy pages, about page, home page

--- e-commerce-app/src/pages/about.tsx
+++ e-commerce-app/src/pages/about.tsx
@@ -1,11 +1,27 @@
 import HeadComponent from "@/components/Head"
+import styles from '../styles/About.module.css'
 
 export default function About(){
     return (
         <>
             <HeadComponent />
-            <div>
-                About
+            <div className={styles.container}>
+                <h1>About Us</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris pulvinar lacinia fermentum. Suspendisse in ex cursus diam condimentum feugiat vitae in enim. Fusce ultricies, risus quis efficitur condimentum, elit nibh dignissim diam, vitae tempor massa mauris ac tellus. Quisque pretium ex non consectetur molestie. Curabitur consequat efficitur magna, nec facilisis dolor malesuada imperdiet. In eget fermentum tortor, sit amet dignissim est. Donec aliquet semper erat id dapibus. Curabitur consequat bibendum leo, elementum viverra purus. Vivamus laoreet dapibus ante eu fringilla. In nec laoreet nulla.
+
+Fusce tempor ut lorem a porttitor. Cras placerat congue malesuada. Suspendisse et hendrerit nunc, a egestas sapien. Morbi ut volutpat magna. Nulla nec egestas nisi, non ullamcorper augue. Maecenas at bibendum libero, eget venenatis elit. Sed nec efficitur enim. Donec a dictum nulla. Donec dignissim molestie metus ultrices dignissim. Sed ultrices, elit et condimentum varius, mi sem molestie justo, nec auctor augue leo at orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam a finibus lacus. Morbi tristique imperdiet odio, sit amet egestas nulla facilisis non.
+
+Integer eget vulputate turpis. Phasellus quis condimentum neque, nec ultricies quam. Phasellus non arcu quis tellus varius rutrum. Mauris eget elit porta, pretium justo et, ultrices quam. Ut sit amet eros id orci pharetra aliquet. Nunc elit erat, pretium non ante a, fermentum efficitur nulla. Pellentesque ornare commodo risus, non semper metus vehicula sagittis.
+
+In sagittis quam at mauris laoreet commodo. Curabitur eget vehicula ligula. Phasellus sed justo et massa faucibus blandit nec ac lacus. Etiam sed dictum lectus. In lobortis consequat commodo. Nam sed molestie quam. Proin vitae nisl sed nibh auctor tempus in non turpis. In hac habitasse platea dictumst. Vivamus lacinia porta dolor, vel maximus ligula fringilla quis. Vivamus mattis aliquet turpis, ac porta augue sodales sed. Donec et pharetra odio, id maximus tortor.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris pulvinar lacinia fermentum. Suspendisse in ex cursus diam condimentum feugiat vitae in enim. Fusce ultricies, risus quis efficitur condimentum, elit nibh dignissim diam, vitae tempor massa mauris ac tellus. Quisque pretium ex non consectetur molestie. Curabitur consequat efficitur magna, nec facilisis dolor malesuada imperdiet. In eget fermentum tortor, sit amet dignissim est. Donec aliquet semper erat id dapibus. Curabitur consequat bibendum leo, elementum viverra purus. Vivamus laoreet dapibus ante eu fringilla. In nec laoreet nulla.
+
+Fusce tempor ut lorem a porttitor. Cras placerat congue malesuada. Suspendisse et hendrerit nunc, a egestas sapien. Morbi ut volutpat magna. Nulla nec egestas nisi, non ullamcorper augue. Maecenas at bibendum libero, eget venenatis elit. Sed nec efficitur enim. Donec a dictum nulla. Donec dignissim molestie metus ultrices dignissim. Sed ultrices, elit et condimentum varius, mi sem molestie justo, nec auctor augue leo at orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam a finibus lacus. Morbi tristique imperdiet odio, sit amet egestas nulla facilisis non.
+
+Integer eget vulputate turpis. Phasellus quis condimentum neque, nec ultricies quam. Phasellus non arcu quis tellus varius rutrum. Mauris eget elit porta, pretium justo et, ultrices quam. Ut sit amet eros id orci pharetra aliquet. Nunc elit erat, pretium non ante a, fermentum efficitur nulla. Pellentesque ornare commodo risus, non semper metus vehicula sagittis.
+
+In sagittis quam at mauris laoreet commodo. Curabitur eget vehicula ligula. Phasellus sed justo et massa faucibus blandit nec ac lacus. Etiam sed dictum lectus. In lobortis consequat commodo. Nam sed molestie quam. Proin vitae nisl sed nibh auctor tempus in non turpis. In hac habitasse platea dictumst. Vivamus lacinia porta dolor, vel maximus ligula fringilla quis. Vivamus mattis aliquet turpis, ac porta augue sodales sed. Donec et pharetra odio, id maximus tortor.
+                </p>
             </div>
         </>
     )
